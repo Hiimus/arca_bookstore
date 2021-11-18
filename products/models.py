cffc36d5a56bdbdbc63465185b1cb0a7490b88b0
Hiimus <hiimhanseneirik@gmail.com>
2021-11-18 11:22:34 +0000

    Added pages

--- products/models.py
+++ products/models.py
@@ -27,6 +27,7 @@ class Book(models.Model):
     author = models.CharField(max_length=260, null=True, blank=True)
     format = models.CharField(max_length=260, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    pages = models.IntegerField(null=True, blank=True)
     book_depository_stars = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
