ba8013984319a11175c857ecd72f7bbb049372b4
Hiimus <hiimhanseneirik@gmail.com>
2021-11-24 15:22:04 +0000

    Added sorting and product counts to books, games and arts & crafts page

--- products/views.py
+++ products/views.py
@@ -14,12 +14,15 @@ def search(request):
     products = Book.objects.filter(Q(name__icontains=q) | Q(description__icontains=q))
     artsandcrafts = ArtsAndCraft.objects.filter(Q(name__icontains=q) | Q(description__icontains=q))
     games = Game.objects.filter(Q(name__icontains=q) | Q(description__icontains=q))
+
+    
     
     context = {
         'products': products,
         'artsandcrafts': artsandcrafts,
         'games': games,
-        'q': q
+        'q': q,
+        'result_count': products.count() + artsandcrafts.count() + games.count()
     }
 
     return render(request, "products/search_products.html", context)
@@ -27,36 +30,116 @@ def search(request):
 
 
 def all_books(request):
-    """ A view to show all products """
+    """ A view to show all Books """
 
     products = Book.objects.all()
 
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products_books.html', context)
 
 
 def all_arts_and_crafts(request):
-    """ A view to show all products """
+    """ A view to show all Arts & Crafts """
 
     artsandcrafts = ArtsAndCraft.objects.all()
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                artsandcrafts = artsandcrafts.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            artsandcrafts = artsandcrafts.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            artsandcrafts = artsandcrafts.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+    current_sorting = f'{sort}_{direction}'
     
     context = {
         'artsandcrafts': artsandcrafts,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products_artsandcraft.html', context)
 
 
 def all_games(request):
-    """ A view to show all products """
+    """ A view to show all games """
 
     games = Game.objects.all()
+    categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                games = games.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            games = games.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            games = games.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+    
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'games': games,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products_games.html', context)
