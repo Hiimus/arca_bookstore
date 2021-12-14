f2588db366ad163fd6262a674acbfdd30967bc16
Hiimus <hiimhanseneirik@gmail.com>
2021-12-14 14:55:16 +0000

    Added includes for home page and randomized products

--- home/views.py
+++ home/views.py
@@ -6,28 +6,61 @@ import random
 # Create your views here.
 
 def index(request):
-    """ A view to return the home page """
+    """ Home page with a randomized sample of products in differents categories """
+    # Code inspired by https://stackoverflow.com/questions/22816704/django-get-a-random-object/23755881#23755881
+    # Get objects by categories
+    book_1 = list(Product.objects.filter(category=1))
+    book_2 = list(Product.objects.filter(category=2))
+    book_3 = list(Product.objects.filter(category=3))
+    book_4 = list(Product.objects.filter(category=4))
 
-    items = list(Product.objects.filter(category=1))
+    arts_and_craft_1 = list(Product.objects.filter(category=11))
+    arts_and_craft_2 = list(Product.objects.filter(category=12))
+    arts_and_craft_3 = list(Product.objects.filter(category=13))
 
-    items2 = list(Product.objects.filter(category=14))
+    game_1 = list(Product.objects.filter(category=14))
+    game_2 = list(Product.objects.filter(category=15))
+    
+    # Get a random sample
+    # change 1 to how many random items you want
+    books_1 = random.sample(book_1, 1)
+    books_2 = random.sample(book_2, 1)
+    books_3 = random.sample(book_3, 1)
+    books_4 = random.sample(book_4, 1)
 
-    items3 = list(Product.objects.filter(category=12))
+    arts_and_crafts_1 = random.sample(arts_and_craft_1, 1)
+    arts_and_crafts_2 = random.sample(arts_and_craft_2, 2)
+    arts_and_crafts_3 = random.sample(arts_and_craft_3, 1)
 
-    # change 3 to how many random items you want
-    products = random.sample(items, 4)
-
-    games = random.sample(items2, 4)
-
-    arts_and_crafts = random.sample(items3, 4)
+    games_1 = random.sample(game_1, 2)
+    games_2 = random.sample(game_2, 2)
 
     context = {
-        'products': products,
-        'games': games,
-        'arts_and_crafts': arts_and_crafts,
+        'books_1': books_1,
+        'books_2': books_2,
+        'books_3': books_3,
+        'books_4': books_4,
+        'arts_and_crafts_1': arts_and_crafts_1,
+        'arts_and_crafts_2': arts_and_crafts_2,
+        'arts_and_crafts_3': arts_and_crafts_3,
+        'games_1': games_1,
+        'games_2': games_2,
     }
 
     return render(request, 'home/index.html', context)
 
 
 
+
+def get_random_books(request):
+    """ A view that gets random books based on categories """  
+
+    
+
+
+    
+
+    context = {
+    }
+
+    return render(request, 'home/index.html', context)
