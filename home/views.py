3ae645782fb54d0226f0c8110c21acc2a79173c7
Hiimus <hiimhanseneirik@gmail.com>
2021-12-21 20:19:43 +0000

    Added width, spacing and improved responsiveness on product cards

--- home/views.py
+++ home/views.py
@@ -17,9 +17,12 @@ def index(request):
     arts_and_craft_1 = list(Product.objects.filter(category=11))
     arts_and_craft_2 = list(Product.objects.filter(category=12))
     arts_and_craft_3 = list(Product.objects.filter(category=13))
+    arts_and_craft_4 = list(Product.objects.filter(category=12))
 
     game_1 = list(Product.objects.filter(category=14))
     game_2 = list(Product.objects.filter(category=15))
+    game_3 = list(Product.objects.filter(category=14))
+    game_4 = list(Product.objects.filter(category=15))
     
     # Get a random sample
     # change 1 to how many random items you want
@@ -29,11 +32,14 @@ def index(request):
     books_4 = random.sample(book_4, 1)
 
     arts_and_crafts_1 = random.sample(arts_and_craft_1, 1)
-    arts_and_crafts_2 = random.sample(arts_and_craft_2, 2)
+    arts_and_crafts_2 = random.sample(arts_and_craft_2, 1)
     arts_and_crafts_3 = random.sample(arts_and_craft_3, 1)
+    arts_and_crafts_4 = random.sample(arts_and_craft_4, 1)
 
-    games_1 = random.sample(game_1, 2)
-    games_2 = random.sample(game_2, 2)
+    games_1 = random.sample(game_1, 1)
+    games_2 = random.sample(game_2, 1)
+    games_3 = random.sample(game_3, 1)
+    games_4 = random.sample(game_4, 1)
 
     context = {
         'books_1': books_1,
@@ -43,8 +49,11 @@ def index(request):
         'arts_and_crafts_1': arts_and_crafts_1,
         'arts_and_crafts_2': arts_and_crafts_2,
         'arts_and_crafts_3': arts_and_crafts_3,
+        'arts_and_crafts_4': arts_and_crafts_4,
         'games_1': games_1,
         'games_2': games_2,
+        'games_3': games_3,
+        'games_4': games_4,
     }
 
     return render(request, 'home/index.html', context)
