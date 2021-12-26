e548b48f46f228f01cf27a7f9cfebda91c0805bf
Hiimus <hiimhanseneirik@gmail.com>
2021-12-26 12:12:46 +0000

    Added edit and delete functionality to blog posts for superusers

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('blogpost/<int:blog_id>/', views.blog_info, name='blog_info'),
     path('post/', views.add_post, name='add_post'),
     path('comment/<int:blog_id>/', views.add_comment, name='add_comment'),
+    path('blogpost/<int:blog_id>/delete/', views.delete_post, name='delete_post'),
+    path('blogpost/<int:blog_id>/edit/', views.edit_post, name='edit_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 from .models import BlogPost, Comment
 from .forms import BlogForm, CommentForm
+
 
 # Create your views here.
 
@@ -45,7 +48,7 @@ def blog_info(request, blog_id):
     return render(request, 'blog/blog_info.html', context)
 
 
-
+@login_required
 def add_post(request):
     """ A view that adds post to blog page """
 
@@ -71,6 +74,49 @@ def add_post(request):
     return render(request, 'blog/view_blogs.html', context)
 
 
+@login_required
+def delete_post(request, blog_id):
+    """ Delete a post from the blog """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('view_blog'))
+        
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    blog.delete()
+    messages.success(request, 'Post deleted!')
+    return redirect(reverse('view_blog'))
+
+
+@login_required
+def edit_post(request, blog_id):
+    """ Edit a post from the blog """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('view_blog'))
+        
+    blog = get_object_or_404(BlogPost, pk=blog_id)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated blog post!')
+            return redirect(reverse('view_blog'))
+        else:
+            messages.error(request, 'Failed to update post. Please ensure the form is valid.')
+    else:
+        form = BlogForm(instance=blog)
+        messages.info(request, f'You are editing {blog.title}')
+
+    template = 'blog/edit_blog.html'
+    context = {
+        'form': form,
+        'blog': blog,
+    }
+    
+    return render(request, template, context)
+
+
+@login_required
 def add_comment(request, blog_id):
     """ A view that adds comments on the blog page """
     
